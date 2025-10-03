a7dfe0b7c462021e525f236f255f4676c620d7e4
Aran Borkum <aran.borkum@kraken.tech>
2025-10-03 10:06:44 +0100

    Move task spawning logic to utils

--- lua/pr-tools/api.lua
+++ lua/pr-tools/api.lua
@@ -154,58 +154,13 @@ end
 function M.mark_pr_as_ready()
 	vim.notify("Marking pull request as ready")
 
-	local stdout = vim.loop.new_pipe(false)
-	local stderr = vim.loop.new_pipe(false)
-
-	local output = {}
-	local errors = {}
-
-	local handle
 	local cmd = { "gh", "pr", "ready" }
 
-	handle = vim.loop.spawn(cmd[1], {
-		args = vim.list_slice(cmd, 2),
-		stdio = { nil, stdout, stderr },
-	}, function(code, _)
-		-- Close handles
-		stdout:close()
-		stderr:close()
-		handle:close()
-
-		vim.schedule(function()
-			if code == 0 then
-				vim.notify("Pull request created:\n" .. table.concat(output, "\n"))
-			else
-				vim.notify("Error creating pull request:\n" .. table.concat(errors, "\n"), vim.log.levels.ERROR)
-			end
-		end)
-	
-		-- Read stdout
-		stdout:read_start(function(err, data)
-			if err then
-				vim.notify(err, vim.log.levels.ERROR)
-				return
-			end
-			if data then
-				for line in data:gmatch("[^\r\n]+") do
-					table.insert(output, line)
-				end
-			end
-		end)
-
-		-- Read stderr
-		stderr:read_start(function(err, data)
-			if err then
-				vim.notify(err, vim.log.levels.ERROR)
-				return
-			end
-			if data then
-				for line in data:gmatch("[^\r\n]+") do
-					table.insert(errors, line)
-				end
-			end
-		end)
-	end)
+	utils.spawn_background_task(
+		cmd,
+		"Pull request marked as ready for review",
+		"Error marking pull request as ready"
+	)
 end
 
 function M.ignore_this()
